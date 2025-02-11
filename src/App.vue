9d4f6e81e901bff2c348ec0077ffca800f7384a1
DmitriyZavgorodniy <dima.zavgorodniy1@gmail.com>
2025-02-11 15:53:21 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Привет, это мой плагин!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Arial, sans-serif;
+}
+</style>
